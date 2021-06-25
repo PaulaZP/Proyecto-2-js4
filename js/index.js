234bc95a0fcf10340139af8bf415177bf89f2c45
PaulaZP <mzp1405mail.com>
2021-06-25 15:22:14 -0600

    se remplazan imagenes, se agrega css y se modifica el js

--- js/index.js
+++ js/index.js
@@ -2,10 +2,12 @@ const form = document.querySelector('#form');
 let counterTask = 0;
 const arrayTask = [];
 const listTask = document.querySelector('#list');
+const optionCategory = document.querySelector('select');
 
 form.addEventListener('submit', (event) =>{
     event.preventDefault();
     handleControls();
+
 })
 
 function handleControls(){
@@ -22,19 +24,47 @@ class ObjectList{
         const item = document.createElement('li');
 
         const checkbox = document.createElement('input');
+        checkbox.className = 'checkbox';
         checkbox.setAttribute('type','checkbox');
         checkbox.setAttribute('id',`task-${counterTask}`);
-        
+        item.appendChild(checkbox);
+
+        if(optionCategory.value === 'shopping'){
+            const shoppingImg = document.createElement('img');
+            shoppingImg.className = 'category-img';
+            shoppingImg.setAttribute("src", "./img/shopping1.png");
+            shoppingImg.setAttribute("alt", "shopping");
+            
+            item.appendChild(shoppingImg);
+    
+        }else if(optionCategory.value === 'chores'){
+            const chores = document.createElement('img');
+            chores.className = 'category-img';
+            chores.setAttribute("src", "./img/chores1.png");
+            chores.setAttribute("alt", "shopping");
+            
+            item.appendChild(chores);
+        }else{
+            const schoolImg = document.createElement('img');
+            schoolImg.className = 'category-img';
+            schoolImg.setAttribute("src", "./img/school1.png");
+            schoolImg.setAttribute("alt", "shopping");
+            
+            item.appendChild(schoolImg);
+        }
+
         const label = document.createElement('label');
         label.setAttribute('for', `task-${counterTask}`);
         label.innerHTML = `${form.elements[0].value}`;
 
-        const deleteImg = document.createElement('img');
-        deleteImg.setAttribute("src", "./img/trashCan1.png");
-        deleteImg.setAttribute("alt", "trash can");
-        item.innerHTML = deleteImg;
+        const editImg = document.createElement('button');
+        editImg.className = 'edit-img clearfix';
+        item.appendChild(editImg);
 
-        item.appendChild(checkbox);
+        const deleteImg = document.createElement('button');
+        deleteImg.className = 'delete-img clearfix';
+        item.appendChild(deleteImg);
+
         item.appendChild(label);
         listTask.appendChild(item);
 
@@ -47,36 +77,6 @@ class ObjectList{
 
         counterTask++;
         form.elements[0].value = '';
+
     }
 }
-    
-
-/*
-const optionCategory = document.querySelector('select');
-const imageCategory = document.querySelector('#imagesCategory');
-
-function category(){
-    if(optionCategory.value === 'shopping'){
-        const shoppingImg = `
-            <img src="./img/shopping1.png" alt="shopping">
-        `
-        imageCategory.innerHTML = shoppingImg;
-
-    }else if(optionCategory.value === 'chores'){
-        const choresImg = `
-            <img src="./img/chores1.png" alt="shopping">
-        `
-        imageCategory.innerHTML = choresImg;
-    }else{
-        const schoolImg = `
-            <img src="./img/school1.png" alt="shopping">
-        `
-        imageCategory.innerHTML = schoolImg;
-    }
-    
-}*/
-
-/* const deleteImg = document.createElement('img');
-        deleteImg.setAttribute("src", "./img/trashCan1.png");
-        deleteImg.setAttribute("alt", "trash can");
-        item.innerHTML = deleteImg;*/
