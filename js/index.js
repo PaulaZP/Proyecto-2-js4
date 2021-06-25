653d9b7cec0de0aa2be228eb01aea8ebb7ba0573
PaulaZP <mzp1405mail.com>
2021-06-24 18:20:46 -0600

    Se modifica codigo para agregar el boton de borrar

--- js/index.js
+++ js/index.js
@@ -6,7 +6,6 @@ const listTask = document.querySelector('#list');
 form.addEventListener('submit', (event) =>{
     event.preventDefault();
     handleControls();
-    category();
 })
 
 function handleControls(){
@@ -14,7 +13,45 @@ function handleControls(){
     createList.add();
 }
 
+class ObjectList{
+    constructor (arrayTask, counterTask )  {
+        this.arrayTask = arrayTask;
+        this.counterTask = counterTask;
+    }
+    add(){
+        const item = document.createElement('li');
 
+        const checkbox = document.createElement('input');
+        checkbox.setAttribute('type','checkbox');
+        checkbox.setAttribute('id',`task-${counterTask}`);
+        
+        const label = document.createElement('label');
+        label.setAttribute('for', `task-${counterTask}`);
+        label.innerHTML = `${form.elements[0].value}`;
+
+        const deleteImg = document.createElement('img');
+        deleteImg.setAttribute("src", "./img/trashCan1.png");
+        deleteImg.setAttribute("alt", "trash can");
+        item.innerHTML = deleteImg;
+
+        item.appendChild(checkbox);
+        item.appendChild(label);
+        listTask.appendChild(item);
+
+        const userTask = {
+            id: counterTask,
+            name: form.elements[0].value,
+            status: false,
+        };
+        arrayTask.push(userTask);
+
+        counterTask++;
+        form.elements[0].value = '';
+    }
+}
+    
+
+/*
 const optionCategory = document.querySelector('select');
 const imageCategory = document.querySelector('#imagesCategory');
 
@@ -37,37 +74,9 @@ function category(){
         imageCategory.innerHTML = schoolImg;
     }
     
-}
+}*/
 
-class ObjectList{
-    constructor (arrayTask, counterTask )  {
-        this.arrayTask = arrayTask;
-        this.counterTask = counterTask;
-    }
-    add(){
-        const item = document.createElement('li');
-
-        const checkbox = document.createElement('input');
-        checkbox.setAttribute('type','checkbox');
-        checkbox.setAttribute('id',`task-${counterTask}`);
-        
-        const label = document.createElement('label');
-        label.setAttribute('for', `task-${counterTask}`);
-        label.innerHTML = `${form.elements[0].value}`;
-
-        item.appendChild(checkbox);
-        item.appendChild(label);
-        listTask.appendChild(item);
-
-        const userTask = {
-            id: counterTask,
-            name: form.elements[0].value,
-            status: false,
-        };
-        arrayTask.push(userTask);
-
-        counterTask++;
-        form.elements[0].value = '';
-    }
-}
-    
+/* const deleteImg = document.createElement('img');
+        deleteImg.setAttribute("src", "./img/trashCan1.png");
+        deleteImg.setAttribute("alt", "trash can");
+        item.innerHTML = deleteImg;*/
