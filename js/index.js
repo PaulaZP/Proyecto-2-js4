73a10ca1628e2444ee60ddddd04033b67d48dd1f
unknown <mzp1405@gmail.com>
2021-06-29 22:13:41 -0600

    se agregan cambios al js y al css

--- js/index.js
+++ js/index.js
@@ -1,53 +1,64 @@
+//Variables principales
 const form = document.querySelector('#form');
-let contadorTareas = 0;
+let counterTask = 0;
 let arrayTask = [];
 const day = new Date();
 day.className = 'day';
 const listTask = document.querySelector('#list');
 const optionCategory = document.querySelector('select');
 
+//evento de tipo submit el cual lee una funcion
 form.addEventListener('submit', (event) =>{
     event.preventDefault();
     handleControls();
-    prueba();
+
     const description = form.elements[0].value;
     console.log(description);
 
     const userTask = {
-        id: contadorTareas,
+        id: counterTask,
         description: description,
-        completo: false,
-        fecha:day,
+        complete: false,
+        date:day,
     }
 
     arrayTask.push(userTask);
-    contadorTareas++;
+    this.counterTask++;
 
     localStorage.setItem('Task', JSON.stringify(arrayTask));
 
     form.elements[0].value = '';
 })
 
+//funcion que controla y ejecuta los métodos del objeto y los instancia al mismo tiempo
 function handleControls(){
-    const createList = new ObjectList(arrayTask);
+    const createList = new ObjectList(arrayTask,day,listTask,optionCategory,counterTask);
     createList.add();    
     createList.edit();
     createList.delete();
 }
-
+//se crea el objeto que contiene varias propiedades se utilizan en los metodos de añadir, editar y eliminar
 class ObjectList{
-    constructor (arrayTask)  {
+    constructor (arrayTask,day,listTask,optionCategory,counterTask)  {
         this.arrayTask = arrayTask;
+        this.day = day;
+        this.listTask = listTask;
+        this.optionCategory = optionCategory;
+        this.counterTask = counterTask;
     }
+
+    /*metodo que crea y agrega los elementos del label conteniendo la 
+    descripcion de la tarea, las opciones de categorias agregando una imagen con el carrito,tareas domesticas
+    y tareas escolares y la hora*/
     add(){
         const item = document.createElement('li');
         
         const checkbox = document.createElement('input');
         checkbox.className = 'checkbox';
-        checkbox.setAttribute('id',`tarea-${contadorTareas}`);
+        checkbox.setAttribute('id',`tarea-${this.counterTask}`);
         checkbox.setAttribute('type','checkbox');
 
-        if(optionCategory.value === 'shopping'){
+        if(this.optionCategory.value === 'shopping'){
             const shoppingImg = document.createElement('img');
             shoppingImg.className = 'category-img';
             shoppingImg.setAttribute('src', './img/shopping1.png');
@@ -55,7 +66,7 @@ class ObjectList{
             
             item.appendChild(shoppingImg);
     
-        }else if(optionCategory.value === 'chores'){
+        }else if(this.optionCategory.value === 'chores'){
             const chores = document.createElement('img');
             chores.className = 'category-img';
             chores.setAttribute('src', './img/chores1.png');
@@ -72,22 +83,25 @@ class ObjectList{
         }
 
         const label = document.createElement('label');
-        label.setAttribute('id',`tarea-${contadorTareas}`);
+        label.setAttribute('id',`tarea-${this.counterTask}`);
         label.innerHTML = `${form.elements[0].value}`;
 
         const hourP = document.createElement('p');
         hourP.className = 'hours clearfix';
-        hourP.innerHTML =`${day.getHours()} : ${day.getMinutes()} : ${day.getSeconds()}`;
+        hourP.innerHTML =`${this.day.getHours()} : ${this.day.getMinutes()} : ${this.day.getSeconds()}`;
 
         const editImg = document.createElement('button');
         editImg.className = 'edit-img clearfix';
 
-        listTask.appendChild(item);
+        this.listTask.appendChild(item);
         item.appendChild(checkbox);
         item.appendChild(label);
         item.appendChild(hourP);
         item.appendChild(editImg);
     }
+
+    /*Metodo el cual sirve para editar la tarea cuando escucha el evento de click del boton 
+    que contiene la imagen de editar*/
     edit(){
         const buttonEdit = document.querySelectorAll('li .edit-img');
         const li = document.querySelectorAll('ul li label');
@@ -109,6 +123,9 @@ class ObjectList{
             })
         }
     }
+
+    /*metodo el cual sirve para elmiminar cada una de las tareas y tambien
+    se pueden eliminar todas al mismo tiempo*/
     delete(){
         const li = document.querySelectorAll('ul li');
         const deleteImg = document.createElement('button');
@@ -133,13 +150,5 @@ class ObjectList{
                 li[i].remove();  
             }
         })
-    }
-}
-
-function prueba(){
-    const tasks = JSON.parse(localStorage.getItem('Task'));
-
-    if(tasks){
-        arrayTask = tasks;
     }
 }
